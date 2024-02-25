dec6ff9a1a2a9ccc4b80efdb78878588a3b4d83c
Douglasmyu <123704099+Douglasmyu@users.noreply.github.com>
2024-02-25 03:45:18 -0800

    Add files via upload

---
+++ skettch.js
@@ -0,0 +1,25 @@
+var bY = 0;
+var score = 0;
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  fill(255);
+  background(0, 0, 0);
+  ellipse(200, bY, 20, 20);
+  bY = bY + 5;
+  
+  if (bY > 400) {
+    bY = 0;
+  }
+
+  text("Your Score:", 300, 50);
+  text(score, 300, 75);
+}
+
+function keyPressed() {
+  if (key == 's' && bY > 350) {
+    score = score + 25;
+  }
+}
