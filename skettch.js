71ff739135cc3feab19469615be36dac266fdb00
sttvbn <112314040+sttvbn@users.noreply.github.com>
2024-02-25 13:25:19 -0800

    new updates

--- skettch.js
+++ skettch.js
@@ -1,3 +1,14 @@
+var start = document.getElementById("start");
+var game = document.getElementById("game"); 
+
+start.addEventListener("click", function(){
+  start.style.display = "none"; 
+  game.style.display = "block"; 
+  setup();
+});
+
+var canvas; 
+
 var bY = 0;
 var aY = 0;
 var dY = 0;
@@ -5,45 +16,54 @@ var fY = 0;
 
 var score = 0;
 function setup() { //this function is for the board
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   fill(255);
-  background(0, 0, 0);
-  ellipse(100, bY, 20, 20); //this will display the ball
-  bY = bY + 5; 
+  background(0, 10);
 
-  ellipse(200, aY, 20, 20);
-  aY = aY +5; 
+  //ellipse(100, bY, 20, 20); //this will display the ball
+  text("s", 100, bY + 30);
+  bY = bY + 1; 
 
-  ellipse(100, dY, 20, 20); //this will display the ball
-  dY = dY + 5; 
+  //ellipse(200, aY, 20, 20);
+  text("a", 140, aY + 30);
+  aY = aY + 1; 
 
-  ellipse(200, fY, 20, 20);
-  fY = fY +5; 
+  //ellipse(100, dY, 20, 20); //this will display the ball
+  text("w", 120, dY + 30);
+  dY = dY + 1; 
+
+  //ellipse(200, fY, 20, 20);
+  text("d", 160, fY + 30);
+  fY = fY + 1; 
 
   if(bY > height) //this determine the ball to reset once it hits the bottom
   {
     bY = 0; 
+    score = score - 25; 
   }
   
   if (aY > height) {
     aY = 0;
+    score = score - 25; 
   }
 
   if(dY > height) //this determine the ball to reset once it hits the bottom
   {
     dY = 0; 
+    score = score - 25; 
   }
   
   if (fY > height) {
     fY = 0;
+    score = score - 25; 
   }
   
 
-  text("Your Score:", 300, 50);
-  text(score, 300, 75);
+  text("Your Score:", width - 100, 50);
+  text(score, width - 100, 75);
 }
 
 function keyPressed() {
@@ -61,6 +81,16 @@ function keyPressed() {
   }
   if (key == 'd' && fY > 350) {
     score = score + 25;
-    fYY = 0;
+    fY = 0;
+  }
+}
+
+
+function game_over(){
+  if (score <= 0){
+    textSize(32);
+    fill(0);
+    textAlign(CENTER);
+    text("GAME OVER!", width / 2, height / 2);
   }
 }
